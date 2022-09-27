160853e7b8bb5e06f7b9d8ae8dbd589871147ccd
vlnk <vlnk@protonmail.com>
2022-09-26 23:44:32 -0400

    ✨ add publishing script

---
+++ publish.el
@@ -0,0 +1,41 @@
+;;; publish.el --- Publish my web identity
+;;; Author: vlnk <vlnk@protonmail.com>
+
+;;; Local Variables:
+;;; End:
+
+;;; Commentary:
+;;; - https://github.com/emacs-mirror/emacs/blob/master/lisp/org/ox-publish.el
+;;; - https://git.sr.ht/~bzg/org-mode/tree/main/item/lisp/ox-md.el
+
+;;; Code:
+(require 'ox-publish)
+
+(setq org-publish-project-alist
+  '(("cv-md-en"
+      :base-directory "."
+      :include ("Curriculum.org")
+      :publishing-directory "public/en"
+      :body-only t
+      :with-title nil
+      :headline-levels 2
+      :select-tags ("en")
+      :publishing-function org-md-publish-to-md)
+     ("cv-html-en"
+       :base-directory "."
+       :include ("Curriculum.org")
+       :publishing-directory "public/en"
+       :body-only t
+       :section-numbers nil
+       :with-title nil
+       :headline-levels 2
+       :select-tags ("en")
+       :publishing-function org-html-publish-to-html)
+     ("cv"
+       :components ("cv-md-en" "cv-html-en"))))
+
+(defun me/publish ()
+  "Publish the web component with org-publish."
+  (org-publish "cv" t))
+
+;;; publish.el ends here
